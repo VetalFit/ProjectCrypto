507eb88329f7df3c63427df407e3907a5fd2b52b
VetalFit <v95664191@gmail.com>
2024-06-23 15:41:54 +0300

    first commit

---
+++ src/components/auth/login/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.css';
+
+const LoginPage = () => {
+	return (
+		<div>
+			<h1 className={style.heading}>Login page</h1>
+		</div>
+	);
+};
+
+export default LoginPage;
